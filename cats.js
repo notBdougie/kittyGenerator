615ec098eb4bab0de36da4ef1fa4be233058c194
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2018-01-07 17:32:12 -0800

    first

---
+++ cats.js
@@ -0,0 +1,9 @@
+// There are over 200 breeds of cats
+const breeds = [
+  "ragdoll",
+  "bengal"
+]
+
+module.exports = {
+  getBreeds: breeds
+}
---
+++ index.js
@@ -0,0 +1,11 @@
+const cats = require("./cats.js");
+const _ = require("lodash");
+
+const breeds = cats.getBreeds;
+
+function randomPrint(list) {
+  const randomBreed = list[_.random(0, list.length - 1)];
+  console.log(randomBreed);
+}
+
+randomPrint(breeds)
